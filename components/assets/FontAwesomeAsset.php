f2569b9cb979d0a18c9c7a0c4d6c21c169a3f36f
arfian <muchammad.arfian@gmail.com>
2016-11-22 21:22:22 +0700

    fa 4.7

--- components/assets/FontAwesomeAsset.php
+++ components/assets/FontAwesomeAsset.php
@@ -18,6 +18,6 @@ use yii\web\AssetBundle;
 class FontAwesomeAsset extends AssetBundle{
     public $sourcePath = null;
     public $css = [
-        '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'
+        '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'
     ];
 }

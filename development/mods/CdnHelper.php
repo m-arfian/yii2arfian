81714f9282863b7e9b2df8c27e3626b7b4babdfe
Arfian <muchammad.arfian@gmail.com>
2018-10-06 21:19:32 +0700

    stash

--- development/mods/CdnHelper.php
+++ development/mods/CdnHelper.php
@@ -18,12 +18,10 @@ use yii\helpers\Url;
 
 class CdnHelper {
     
-    public static function getUrl($path, $isAbsolute = true, $cdnHosted = false) {
-        $param = \Yii::$app->params['s3cdn'];
-        $cdnPath = "http://{$param['bucket']}{$param['host']}" . Url::to($path);
-            
-        if ($cdnHosted /* && file_exists($cdnPath) */) {
-            return $cdnPath;
+    public static function getUrl($path, $isAbsolute = false, $cdnHosted = true) {
+        if ($cdnHosted) {
+            $param = \Yii::$app->params['s3cdn'];
+            return "http://{$param['bucket']}{$param['host']}" . Url::to($path);
         } else {
             return Url::to($path, $isAbsolute);
         }
